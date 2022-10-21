d49bd91f3d019e5b917c818f76487c06f772ca5e
@sebastiangancino <sjgancino97sd@gmail.com>
2022-10-20 23:22:54 -0500

    26:30

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
-import "./App.css";
 import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import Intro from "./components/Intro/Intro";
 
 function App(){
   return (
+    <div className='App'>
     <Navbar/>
+    <Intro />
+    </div>
   );
+  
 }
 
 export default App;
