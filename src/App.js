92fd4239accb26a38dfe02e9e0862308c2628673
@sebastiangancino <sjgancino97sd@gmail.com>
2022-11-21 00:42:14 -0500

    Todo bien hasta el minuto 38

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import Navbar from './components/Navbar/Navbar';
 import "./App.css";
 import Intro from "./components/Intro/Intro";
-
+import Test from "./components/Intro/test"
 function App(){
   return (
     <div className='App'>
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './Intro.css';
 import  github from '../../../src/assets/img/github.png';
+import ellipse1 from '../../assets/img/Ellipse 1.png';
+import ellipse2 from '../../assets/img/Ellipse 2.png';
+import perfil from '../../assets/img/perfil-portafolio.png';
 
 const Intro = () => {
   return (
@@ -17,8 +20,15 @@ const Intro = () => {
               <img src="https://cdn-icons-png.flaticon.com/512/3938/3938061.png" alt="" />
             </div>
         </div>
-        <div className="i-right">Rigthfsdfdsfdsfdsfsd </div>
+        <div className="i-right">
+        <img src= {ellipse2} alt="" className='el2' />
+          <img src= {ellipse1} alt=""  className='el1'/>  
+          <img src= {perfil} alt="" className='perfil'/> 
+
+        </div>
+      
     </div>
+
   )
 }
 
