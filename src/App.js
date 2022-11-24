8e15e4be649d11c656d059999bdccfdbb6928703
@sebastiangancino <sjgancino97sd@gmail.com>
2022-11-24 12:20:15 -0500

    Todo bien min 59

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import Navbar from './components/Navbar/Navbar';
 import "./App.css";
 import Intro from "./components/Intro/Intro";
-import Test from "./components/Intro/test"
+import Test from "./components/Intro/test";
+import Services from './components/Servicess/Services';
 function App(){
   return (
     <div className='App'>
     <Navbar/>
     <Intro />
+    <Services/>
     </div>
   );
   
