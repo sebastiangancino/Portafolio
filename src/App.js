0907e40353a8f8f97435a30d3857ceea2b63d812
@sebastiangancino <sjgancino97sd@gmail.com>
2022-12-10 01:34:04 -0500

    Todo bien 1:16:00

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import Intro from "./components/Intro/Intro";
 import Test from "./components/Intro/test";
 import Services from './components/Servicess/Services';
+import Experiencie from './components/Experience/Experiencie';
 function App(){
   return (
     <div className='App'>
     <Navbar/>
     <Intro />
     <Services/>
+    <Experiencie/>
     </div>
   );
   
--- src/components/Servicess/Services.jsx
+++ src/components/Servicess/Services.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import '../Servicess/Services.css';
 import Card from '../Card/Card';
 import web from '../../assets/img/web-design.png'
-
-
+import glasses from '../../assets/img/sunglasses.png'
+import cv from './cv.pdf'
 
   const Services =() => {
     return (
@@ -17,23 +17,50 @@ import web from '../../assets/img/web-design.png'
             <br/>
             ipsinfds f ds fds f ds f sd fs d fsd
             </span>
-            <button className="button s-button">Download CV</button>
-            <div className="blur s-blur" style= {{background: "#ABF1FF94"}}></div>
+
+            <a href= {cv} target = 'blank'>
+             <button className="button s-button">Download CV</button>
+            </a>
+            <div className="blur s-blur1" style= {{background: "#ABF1FF94"}}></div>
         
         </div>
         
         {/* rigth side */}
         <div className='cards'>
           
-          <div>
+          <div style={{left: '14rem'}}>
           <Card
           emoji = {web}
           heading = {"Desing"}
           detail = {"Figma, CSS, HTLM5, REACT, NPM, SQL"}
           />
         </div>
+
+         {/* second card */}
+         <div style={ {top: '12rem', left:'-4rem'}}>
+         <Card
+          emoji = {glasses}
+          heading = {"Desing"}
+          detail = {"Figma, CSS, HTLM5, REACT, NPM, SQL"}
+          />
+         </div>
+
+         {/* 3th card */}
+         <div style={ {top: '24rem', left:'14rem'}}>
+         <Card
+          emoji = {glasses}
+          heading = {"Desing"}
+          detail = {"Figma, CSS, HTLM5, REACT, NPM, SQL"}
+          />
+                   <div className="blur s-blur2" style= {{background: "var(--blue)"}}></div>
+
+         </div>
+
         </div>
+        
         </div>
+
+        
 
     )
     }
