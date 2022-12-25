66e463ab425f2984b45c651418b0d1c46e13c507
@sebastiangancino <sjgancino97sd@gmail.com>
2022-12-25 14:48:51 -0500

    Todo bien min 1:21

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../Experience/Experience.css'
+
+function Experience() {
+  return (
+    <div className='experience'>
+      <div className="exp-cards">
+
+        <div className="ex-card  ex-card1">
+           <div className="circle">33%</div>
+            <span>Desarrollo</span>
+            <br />
+            <span>Frontend</span>
+        </div>
+
+        <div className="ex-card  ex-card2">
+           <div className="circle">33%</div>
+            <span>Desarrollo</span>
+            <br />
+            <span>Frontend</span>
+        </div>
+
+        <div className="ex-card  ex-card3">
+           <div className="circle">33%</div>
+            <span>Desarrollo</span>
+            <br />
+            <span>Frontend</span>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/components/Works/Works.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../Works/Works.css'
+
+function Works() {
+  return (
+    <div className='works'>
+       <div className="awesome">
+            <span>Works for all these</span>
+            <span>Brands and Clients</span>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore totam iure, quidem voluptas quae aut inventore eius vel quas reprehenderit beatae quibusdam ex dolorem illum. Doloribus cupiditate similique ab dolorem.
+            <br/>
+            ipsinfds f ds fds f ds f sd fs d fsd
+            </span>
+
+             <button className="button s-button">Download CV</button>
+            <div className="blur s-blur1" style= {{background: "#ABF1FF94"}}></div>
+            <br />
+            <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque molestias incidunt impedit quaerat consequatur. Est deleniti voluptas vel nihil, amet cum itaque laudantium soluta sit fugiat? Dolor incidunt nam quibusdam!</span>
+        </div>
+    </div>
+  )
+}
+
+export default Works
